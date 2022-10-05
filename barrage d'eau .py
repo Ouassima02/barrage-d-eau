d8f1512fe9911e6b92e376f5a5a1ec4b4f3e186a
Ouassima02 <115112884+Ouassima02@users.noreply.github.com>
2022-10-05 22:13:33 +0200

    paramètres barrage d'eau

---
+++ barrage d'eau .py
@@ -0,0 +1,8 @@
+mv=float(input("entrez la valeur de la masse volumique:"))
+h=float(input("entrez la valeur de la hauteur:"))
+g=float(input("entrez la valeur de l'accélération:"))
+P=mv*h*g
+print(P)
+L=float(input("entrez la valeur de la longueur:"))
+F=P*h*L
+print(F)
